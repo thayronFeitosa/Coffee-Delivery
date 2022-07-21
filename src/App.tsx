cb08b9489817c44d94724a8248ce17194adfb5fa
thayron.matos <thayronf3@gmail.com>
2022-07-21 13:15:09 -0300

    created input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -14,6 +15,7 @@ export function App() {
       <div className="App">
         <h1>hello word</h1>
         <Button onClick={handleClick}>Label</Button>
+        <Input placeholder="sadfas" />
       </div>
       <GlobalStyle />
     </ThemeProvider>
